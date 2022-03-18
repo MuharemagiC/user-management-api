621f270a6c91c9b1ec61f57724f4bcfdf8d51193
MuharemagiC <emir.rggf.muharemagic.97@gmail.com>
2022-03-18 20:25:31 +0100

    Added body parser to server.js file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const userRoutes = require('./routes/userRoutes')
 
-const app = express()
 const PORT = process.env.PORT || 3002
+
+const app = express()
+app.use(bodyParser.json())
 
 app.use('/user', userRoutes)
 
