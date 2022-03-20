59d721864c5cf6fc8472de1f1fbb3a78dafdc306
MuharemagiC <emir.rggf.muharemagic.97@gmail.com>
2022-03-20 14:57:08 +0100

    Fixed production cors problem

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const userRoutes = require('./routes/userRoutes')
 const rolesRoutes = require('./routes/rolesRoutes')
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3002
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors({ origin: "https://emir-user-management-api.herokuapp.com" }))
 
 app.use('/user', userRoutes)
 app.use('/roles', rolesRoutes)
